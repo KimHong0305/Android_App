f4296cea69c91b75160b56fa34cc3c578f63e4e4
KimHong0305 <145215715+KimHong0305@users.noreply.github.com>
2025-01-13 15:06:37 +0700

    Bài tập tuần 2

--- App.tsx
+++ App.tsx
@@ -1,17 +1,48 @@
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import WelcomeScreen from "./screens/WelcomeScreen";
-import HomeScreen from "./screens/HomeScreen";
+import React, { useEffect, useState } from 'react';
+import { SplashScreen } from './src/screens';
+import { NavigationContainer } from '@react-navigation/native';
+import AuthNavigator from './src/navigators/AuthNavigator';
+import MainNavigator from './src/navigators/MainNavigator';
+import { StatusBar } from 'react-native';
+import { useAsyncStorage } from '@react-native-async-storage/async-storage';
 
-const Stack = createNativeStackNavigator();
+const App = () => {
+  const [isShowSplash, setIsShowSplash] = useState(true);
+  const [accessToken, setAccessToken] = useState('');
 
-export default function App() {
+  const { getItem, setItem } = useAsyncStorage('assetToken');
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setIsShowSplash(false);
+    }, 1500);
+
+    return () => clearTimeout(timeout);
+  }, []);
+
+  useEffect(() => {
+    checkLogin();
+  }, []);
+
+  const checkLogin = async () => {
+    const token = await getItem();
+    if (token) {
+      setAccessToken(token);
+    }
+  };
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Welcome">
-        <Stack.Screen name="Welcome" component={WelcomeScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <>
+      <StatusBar barStyle={'dark-content'} translucent backgroundColor={'transparent'} />
+      {isShowSplash ? (
+        <SplashScreen />
+      ) : (
+        <NavigationContainer>
+          {accessToken ? <MainNavigator /> : <AuthNavigator />}
+        </NavigationContainer>
+      )}
+    </>
   );
-}
+};
+
+export default App;
---
+++ src/navigators/AuthNavigator.tsx
@@ -0,0 +1,20 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from 'react';
+import { LoginScreen, OnboardingScreen } from '../screens';
+
+const AuthNavigator = () => {
+    const Stack = createNativeStackNavigator();
+
+    return (
+        <Stack.Navigator
+            screenOptions={{
+                headerShown: false,
+            }}
+        >
+            <Stack.Screen name="OnboardingScreen" component={OnboardingScreen} />
+            <Stack.Screen name="LoginScreen" component={LoginScreen} />
+        </Stack.Navigator>
+    );
+};
+
+export default AuthNavigator;
